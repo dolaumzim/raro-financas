8e77342b90d3a0151402d163dd7de9bae58a6ed5
armando.assini <arm.assini@gmail.com>
2023-08-12 14:08:24 -0300

    closes #2 - bg criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { Header } from './Components/Header'
 
 function App() {
 
-  return <div>  </div>
+  return <Header/>;
+    
 }
 
 export default App
---
+++ src/Components/RaroFinancas/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+interface props{
+    children: React.ReactNode;
+}
+
+export const RaroFinancas = (props : props) =>{
+    return(
+        <div className="raroFinancas">
+            {props.children}
+        </div>
+    )
+}
